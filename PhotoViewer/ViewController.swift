e5e37bf5f197228c1345a46c146fbf0ec7336cbf
Alex Walker <alexwalker@Alexs-MacBook-Pro.local>
2015-06-15 20:41:29 -0700

    Initial Commit

---
+++ PhotoViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PhotoViewer
+//
+//  Created by Alex Walker on 6/15/15.
+//  Copyright (c) 2015 Alex Walker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
